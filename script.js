91b55b383470196e4712529d823ed23e65d5e7d8
Viktor <victorosetrov@github.com>
2021-02-13 19:05:58 +0000

    Added close/open modals

---
+++ script.js
@@ -0,0 +1,102 @@
+'use strict';
+/*
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const displayBackground = function (body) {
+  document.querySelector('body').style.backgroundColor = body;
+};
+
+const displayWidth = function (width) {
+  document.querySelector('.number').style.width = width;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When no input
+  if (!guess) {
+    displayMessage('🐷 Try again, buddy!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🥳 You are winner!');
+    displayNumber(secretNumber);
+    displayBackground('#60b347');
+    displayWidth('30rem');
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber
+          ? '🔥 Too hot, colder please'
+          : '🥶 Too cold, hotter pls!'
+      );
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('😭 Game over');
+      displayScore(0);
+      displayBackground('#8b0000');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  displayScore(score);
+  displayNumber('?');
+  document.querySelector('.guess').value = '';
+  displayBackground('#222');
+  displayWidth('15rem');
+});
+
+*/
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
